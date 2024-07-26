9f5d63c4db8ee093524f78ab12825ad847edd603
Dreven95 <tdreven@mail.ru>
2024-07-26 15:54:16 +0300

    Commit

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,27 +1,28 @@
 const URL = "http://localhost:8080/api";
 
 $(document).ready(function() {
+    // Fetch roles from the server
     fetch(URL + '/roles')
         .then(response => response.json())
         .then(data => {
-            const rolesSelect = $('#roles');
+            const rolesSelect = $('#new-roles'); // Убедитесь, что ID совпадает с HTML
             data.forEach(role => {
                 rolesSelect.append(new Option(role.name, role.id));
             });
         });
 
-
+    // Form submission handler
     $('#new-user-form').on('submit', function(event) {
         event.preventDefault();
 
         const formData = {
-            name: $('#name').val(),
-            email: $('#email').val(),
-            password: $('#password').val(),
-            roles: $('#roles').val()
+            email: $('input[name="newUserEmail"]').val(),
+            name: $('input[name="newUserName"]').val(),
+            password: $('input[name="newUserPassword"]').val(),
+            roles: $('#new-roles').val() // Получаем выбранные роли
         };
 
-        fetch('/admin/saveUser', {
+        fetch(URL + '/users', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
